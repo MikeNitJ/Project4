6739fce0f85c703bbdf7f91af3021fd02917821b
mikenitj <mikenitipong@hotmail.com>
2023-11-08 04:06:36 +0700

    backend...

--- hangman/views.py
+++ hangman/views.py
@@ -1,13 +1,15 @@
 from rest_framework import generics
 from .models import Scoreboard
 from .serializers import ScoreboardSerializer
-
+from rest_framework import permissions
 # Create your views here.
 
 class ScoreboardList(generics.ListCreateAPIView):
     queryset = Scoreboard.objects.all()
     serializer_class = ScoreboardSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class ScoreboardDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Scoreboard.objects.all()
     serializer_class = ScoreboardSerializer
+    permission_classes = [permissions.IsAuthenticated]
